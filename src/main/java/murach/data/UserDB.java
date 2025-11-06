b12506d1369a1dcc6e2c58e0ff6180055328928b
Xuan Huy <23110102@student.hcmute.edu.vn>
2025-11-06 14:39:41 +0700

    updated userdb to just only print not save for better cloud deploy

--- src/main/java/murach/data/UserDB.java
+++ src/main/java/murach/data/UserDB.java
@@ -7,13 +7,7 @@ public class UserDB {
     private static final String filePath = "C:/data/users.txt";
 
     public static void insert(User user) {
-        try (PrintWriter out = new PrintWriter(
-                new FileWriter(filePath, true))) {
-            out.println(user.getEmail() + "|" +
-                    user.getFirstName() + "|" +
-                    user.getLastName());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Inserting user: " + user);
+
     }
 }
